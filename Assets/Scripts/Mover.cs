1e5046e6a6e61fae2ea59a5d5bb60811d4b4dce6
amclv <toscleveland@gmail.com>
2021-08-20 01:31:25 -0500

    Added RayCasting

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,15 +9,18 @@ public class Mover : MonoBehaviour
     [SerializeField]
     private Transform target;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    Ray lastRay;
+
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
+
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
