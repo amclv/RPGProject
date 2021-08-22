50bf4016b5e66f721b842a2a32a6192851a97c87
amclv <toscleveland@gmail.com>
2021-08-22 02:07:17 -0500

    Dependency Inversion with Interfaces

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -5,7 +5,7 @@ using RPG.Core;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour
+    public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
         Transform target;
@@ -14,13 +14,13 @@ namespace RPG.Combat
         {
             if (target == null) return;
 
-            if (target != null && !GetIsInRange())
+            if (!GetIsInRange())
             {
                 GetComponent<Mover>().MoveTo(target.position);
             }
             else
             {
-                GetComponent<Mover>().Stop();
+                GetComponent<Mover>().Cancel();
             }
         }
 
