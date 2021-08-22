f9bdfe3eed3c5c35928808b053a62a5e95b293cb
amclv <toscleveland@gmail.com>
2021-08-21 22:24:25 -0500

    Raycasting for components

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attack(CombatTarget target)
+        {
+            Debug.Log("ATTACK!");
+        }
+    }
+}
