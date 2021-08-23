51720f9eb70c00feadea619bfd177f30993d18a1
amclv <toscleveland@gmail.com>
2021-08-22 19:11:57 -0500

    Throttling attacks

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -8,10 +8,15 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -21,13 +26,17 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
-                Attack();
+                AttackBehaviour();
             }
         }
 
-        private void Attack()
+        private void AttackBehaviour()
         {
-            GetComponent<Animator>().SetTrigger("attack");
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
         }
 
         private bool GetIsInRange()
